e98dfa0bcf0af2c58e4b11803155918df5d2f604
sa46lll <9812smh@naver.com>
2022-01-27 09:56:50 +0900

    feat: Greeting 예제

--- src/main/java/com/example/docker/Application.java
+++ src/main/java/com/example/docker/Application.java
@@ -2,12 +2,11 @@ package com.example.docker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
-public class DockerApplication {
-
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(DockerApplication.class, args);
+        SpringApplication.run(Application.class, args);
+        GreetingWebClient gwc = new GreetingWebClient();
+        System.out.println(gwc.getResult());
     }
-
 }
